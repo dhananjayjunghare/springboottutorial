7f14969a3c3ec77b2efdf7b28bdc38d5d4310118
Dhananjay <dhananjayjunghare23@gmail.com>
2023-11-01 09:18:03 +0530

    first commit

---
+++ src/test/java/com/hibernate/hibernateonetoonebi/HibernateOneToOneBiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hibernate.hibernateonetoonebi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateOneToOneBiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
